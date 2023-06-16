6a2c626696ed4f08de5d8f67b575f7ff59767f3d
DataKinds <tslimkemann42@gmail.com>
2023-06-15 19:00:16 -0700

    Prepare package for pushing to PyPI

--- src/pycoparsec/__about__.py
+++ src/pycoparsec/__about__.py
@@ -1,4 +1,4 @@
 # SPDX-FileCopyrightText: 2023-present DataKinds <tslimkemann42@gmail.com>
 #
-# SPDX-License-Identifier: MIT
+# SPDX-License-Identifier: LGPL-3.0-or-later
 __version__ = "0.0.1"
--- tests/__init__.py
+++ tests/__init__.py
@@ -1,3 +1,3 @@
 # SPDX-FileCopyrightText: 2023-present DataKinds <tslimkemann42@gmail.com>
 #
-# SPDX-License-Identifier: MIT
+# SPDX-License-Identifier: LGPL-3.0-or-later
